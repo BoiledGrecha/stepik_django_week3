ae298c5a8709df5618c724d3ee6b142e15a34add
BoiledGrecha <40800324+BoiledGrecha@users.noreply.github.com>
2021-04-12 14:34:03 +0300

    Change allowed hosts

--- jumanji/settings.py
+++ jumanji/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0f53$!8bjv!!gsgr776qg69udt1jiype*p60@ro=jzr64xfph-'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['*']
 
 INTERNAL_IPS = [
     '127.0.0.1',
